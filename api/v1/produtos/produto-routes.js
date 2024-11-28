2ae9f9f087d6d2390c87f8c17fd54ec89ae8643c
Metronox <samuksantos1235@gmail.com>
2024-11-28 18:55:33 -0300

    Criação das rotas

---
+++ api/v1/produtos/produto-routes.js
@@ -0,0 +1,40 @@
+const produtoHandler = require('./produto-handler');
+const produtoSchema = require('./produto-schema');
+
+module.exports = [
+  {
+    method: 'POST',
+    path: '/produtos',
+    handler: produtoHandler.criarProduto,
+    options: {
+      validate: {
+        payload: produtoSchema,
+      },
+    },
+  },
+  {
+    method: 'PUT',
+    path: '/produtos/{id}',
+    handler: produtoHandler.atualizarProduto,
+    options: {
+      validate: {
+        payload: produtoSchema,
+      },
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/produtos/{id}',
+    handler: produtoHandler.removerProduto,
+  },
+  {
+    method: 'GET',
+    path: '/produtos/{id}',
+    handler: produtoHandler.obterProdutoPorId,
+  },
+  {
+    method: 'GET',
+    path: '/produtos',
+    handler: produtoHandler.filtrarProdutos,
+  },
+];
